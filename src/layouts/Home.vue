e88265de5c1db8481b76ce78855f5edd6808ed25
alexsfig <alexandersosa2204@gmail.com>
2018-04-30 02:06:37 -0600

    add default skeleton

---
+++ src/layouts/Home.vue
@@ -0,0 +1,47 @@
+<template>
+<div class="">
+  <header>
+    <div class="collapse bg-dark" id="navbarHeader">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-md-7 py-4">
+            <h4 class="text-white">About</h4>
+            <p class="text-muted">Snack store.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="navbar navbar-dark bg-dark box-shadow">
+      <div class="container d-flex justify-content-between">
+        <a href="#" class="navbar-brand d-flex align-items-center">
+          <img src="../assets/images/logo2.png" alt="" class="img-fluid" style="max-width: 40px;">
+          <strong>&nbsp;&nbsp;Snack Store</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </div>
+  </header>
+  <main role="main">
+    <router-view/>
+  </main>
+</div>
+
+</template>
+
+
+
+<style lang="css">
+
+  @import '../assets/bower_components/font-awesome/css/font-awesome.min.css';
+  @import '../assets/bower_components/Ionicons/css/ionicons.min.css';
+  .breadcrumb > li { width: auto !important }
+  .help-block{ color: #dd4b39 !important; }
+  .has-error { color: #dd4b39 !important }
+  .btn.has-error { border-color: #dd4b39 !important }
+  .table > tbody > tr > td {
+       vertical-align: middle;
+  }
+  .margin { margin: 5px; }
+</style>
---
+++ src/main.js
@@ -0,0 +1,122 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import * as uiv from 'uiv'
+import moment from 'moment'
+import {HTTP} from './common_class/http.js';
+import esp from 'vee-validate/dist/locale/es';
+import VueGoodTable from 'vue-good-table'
+/* eslint-disable no-new */
+import VeeValidate, { Validator } from 'vee-validate';
+import VueMask from 'v-mask'
+import VueFormWizard from 'vue-form-wizard'
+import 'vue-form-wizard/dist/vue-form-wizard.min.css'
+import VTooltip from 'v-tooltip'
+import round from 'vue-round-filter';
+import Toasted from 'vue-toasted';
+import * as VueGoogleMaps from 'vue2-google-maps';
+import FullCalendar from 'vue-full-calendar'
+import "fullcalendar/dist/fullcalendar.min.css";
+
+Vue.use(FullCalendar)
+
+Vue.config.productionTip = false;
+
+Vue.use(VueGoogleMaps, {
+  load: {
+    key: 'AIzaSyDRuXnqROzf88kyefcZVSlhPkapyknqUTM',
+    libraries: 'places', // This is required if you use the Autocomplete plugin
+    // OR: libraries: 'places,drawing'
+    // OR: libraries: 'places,drawing,visualization'
+    // (as you require)
+  }
+})
+Vue.use(VueFormWizard)
+Vue.use(VueMask);
+Vue.use(VueGoodTable)
+Vue.use(VTooltip)
+import ToggleButton from 'vue-js-toggle-button'
+Vue.use(ToggleButton)
+
+import VueSweetAlert from 'vue-sweetalert'
+Vue.use(VueSweetAlert)
+Vue.use(uiv)
+Vue.use(round)
+Vue.config.productionTip = false
+Vue.use(Toasted)
+router.beforeEach((to, from, next) => {
+
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        // this route requires auth, check if logged in
+        // if not, redirect to login page.
+
+        if (localStorage.authorization == null) {
+          next({
+            path: '/login',
+            query: { redirect: to.fullPath }
+          })
+        } else {
+          HTTP.get('persona')
+                .then((resp) => {
+                    next()
+                })
+                .catch((err) => {
+                  localStorage.removeItem('authorization')
+                    next({
+                        path: '/login',
+                        query: {
+                            redirect: to.fullPath
+                        }
+                    })
+                })
+        }
+      }
+
+  else {
+    next() // make sure to always call next()!
+  }
+  if (!to.matched.length) {
+    next('/404');
+  } else {
+    next();
+  }
+})
+
+
+Vue.filter('formatDate', function(value) {
+  if (value) {
+    return moment(String(value)).format('YYYY-MM-DD')
+  }
+})
+
+Vue.filter('formatDateTime', function(value) {
+  if (value) {
+    return moment(String(value)).format('YYYY-MM-DD hh:mm')
+  }
+})
+
+// Merge the locales.
+Validator.localize('es', esp);
+
+const config = {
+    locale: 'es'
+};
+
+Vue.use(VeeValidate, config);
+
+/* eslint-disable no-new */
+new Vue({
+  filters: {
+    round,
+  },
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App },
+  data: () => ({
+    email: '',
+    phone: ''
+  }),
+})
